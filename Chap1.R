b93335b63f948d874d910d8c6c3f314dac93ff24
Lucia <jayoung82.kim@gmail.com>
2024-08-18 12:55:19 +0900

    .

--- Chap1.R
+++ Chap1.R
@@ -114,3 +114,69 @@ write.csv(dat3,"subset_data.csv")
 write.table(dat3,"subset_data.txt")
 save(dat3,file="subset_data.RData")
 load("subset_data.RData")
+
+#dplyr Package
+library(dplyr)
+x <- 1:10
+sum(x)
+x %>% sum()
+
+sum(round(log(x)))
+x %>% log() %>% round() %>% sum()
+sum(round(log(x),digits = 1))
+x %>% log() %>% round(digits = 1) %>% sum()
+
+dat.base1 <- dat1
+colnames(dat1)
+colnames(dat.base1)[4] <-"TX"
+colnames(dat.base1)[5] <- "CA19.9"
+
+dat.base1.1 <- dat1
+colnames(dat.base1.1)[colnames(dat.base1.1)=="treatment"] <-"TX"
+colnames(dat.base1.1)[colnames(dat.base1.1)=="CA19-9"] <-"CA19.9"
+colnames(dat.base1.1)
+dat.dplyr1 <- dat1 %>% rename(TX=treatment, CA19.9="CA19-9")
+dat.dplyr1
+
+dat.base2 <- dat.base1[,c(2:5,7)]
+dat.base2
+dat.base2.1 <- dat.base1[,-c(1,6,8)]
+dat.base2.1
+dat.base2.2 <- dat.base1[,c("age","sex","TX","CA19.9","Stage")]
+dat.base2.2
+dat.dplyr2 <- dat.dplyr1 %>% select("age","sex","TX","CA19.9","Stage")
+dat.dplyr2
+dat.dplyr2.1 <- dat.dplyr1 %>% select(,c(2:5,7))
+dat.dplyr2.1
+dat.dplyr2.2 <- dat.dplyr1 %>% select(,-c(1,6,8))
+dat.dplyr2.2
+dat.dplyr2.3 <- dat.dplyr1 %>% select(-id,-CRP,-complication)
+dat.dplyr2.3
+
+dat.base3 <- dat.base2[dat.base2$age>=40,]
+dat.base3
+dat.base3.1 <- dat.base2[dat.base2$Stage==3,]
+dat.base3.1
+dat.base3.2 <- dat.base2[dat.base2$age>=40&dat.base2$Stage==3,]
+dat.base3.2
+dat.base3.3 <- dat.base2[dat.base2$age>=40 | dat.base2$Stage==3,]
+dat.base3.3
+
+dat.dplyr3 <- dat.dplyr2 %>% filter(age>=40)
+dat.dplyr3
+dat.dplyr3.1 <- dat.dplyr2 %>% filter(Stage==3)
+dat.dplyr3.1
+dat.dplyr3.2 <- dat.dplyr2 %>% filter(age>=40 & Stage==3)
+dat.dplyr3.2
+dat.dplyr3.3 <- dat.dplyr2 %>% filter(age>=40 | Stage ==3)
+dat.dplyr3.3
+
+typeof(dat1)
+dat.base4 <- dat.base3
+dat.base4$CA19.9[dat.base4$CA19.9=="<1.0"] <-"1"
+dat.base4
+dat.base4.1 <- dat.base3
+dat.base4.1$CA19.9 <- replace(dat.base4$CA19.9, dat.base4.1$CA19.9=="<1.0",1)
+dat.base4.1
+dat.dplyr4 <- dat.dplyr3 %>% mutate(CA19.9=replace(CA19.9,CA19.9=="<1.0",1))
+dat.dplyr4
